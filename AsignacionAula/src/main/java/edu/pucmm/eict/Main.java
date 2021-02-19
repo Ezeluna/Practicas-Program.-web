89795dd69f5a51310e13a1f97e151806172f6700
Ezequiel Luna <ez3quielluna02@gmail.com>
2021-02-19 18:29:21 -0400

    creando filtros

--- AsignacionAula/src/main/java/edu/pucmm/eict/Main.java
+++ AsignacionAula/src/main/java/edu/pucmm/eict/Main.java
@@ -1,4 +1,33 @@
 package edu.pucmm.eict;
+import io.javalin.Javalin;
+import io.javalin.core.util.RouteOverviewPlugin;
+
+import javax.naming.ldap.Control;
 
 public class Main {
+    public static void main(String[] args){
+        Javalin app = Javalin.create(config->{
+            config.addStaticFiles("/resources");
+            config.registerPlugin(new RouteOverviewPlugin("/"));
+        });
+
+        app.start(getHerokuAssignedPort());
+
+
+
+
+    }
+
+
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 7000;
+    }
+
+
+
 }
