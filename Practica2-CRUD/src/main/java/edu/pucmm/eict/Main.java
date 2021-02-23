f548e2aa2b700d27412b2467531b00791241be32
Ezequiel Luna <ez3quielluna02@gmail.com>
2021-02-23 16:33:12 -0400

    Arreglando detalles

--- Practica2-CRUD/src/main/java/edu/pucmm/eict/Main.java
+++ Practica2-CRUD/src/main/java/edu/pucmm/eict/Main.java
@@ -5,7 +5,7 @@ import io.javalin.Javalin;
 public class Main {
     public static void main(String[] args){
         Javalin app = Javalin.create(config ->{
-
-        } );
+            config.addStaticFiles("/public");
+        }).start(7000);
     }
 }
