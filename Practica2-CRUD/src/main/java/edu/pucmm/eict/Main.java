1469495a4f1eb5260d819a991fa70a5db0d7d2ac
Ezequiel Luna <ez3quielluna02@gmail.com>
2021-02-23 19:02:35 -0400

    Modificando en login y templates + agregando plantillas

--- Practica2-CRUD/src/main/java/edu/pucmm/eict/Main.java
+++ Practica2-CRUD/src/main/java/edu/pucmm/eict/Main.java
@@ -1,11 +1,15 @@
 package edu.pucmm.eict;
 
+import edu.pucmm.eict.controladores.Template;
 import io.javalin.Javalin;
 
 public class Main {
-    public static void main(String[] args){
-        Javalin app = Javalin.create(config ->{
-            config.addStaticFiles("/public");
+    public static void main(String[] args) {
+        Javalin app = Javalin.create(config -> {
+            config.addStaticFiles("/templates");
         }).start(7000);
+
+        new Template(app).aplicarRutas();
+
     }
 }
--- Practica2-CRUD/src/main/java/edu/pucmm/eict/controladores/Template.java
+++ Practica2-CRUD/src/main/java/edu/pucmm/eict/controladores/Template.java
@@ -5,15 +5,24 @@ import io.javalin.plugin.rendering.JavalinRenderer;
 import io.javalin.plugin.rendering.template.JavalinThymeleaf;
 
 public class Template {
-    private Javalin app;
+    Javalin app;
 
-    public void templateControlador(Javalin app){
+    public Template(Javalin app){
         this.app = app;
         registrandoTemplate();
     }
 
     public void registrandoTemplate(){
         JavalinRenderer.register(JavalinThymeleaf.INSTANCE, ".html");
+    }
+
+    public void aplicarRutas(){
+        //app.get("/", ctx -> ctx.result("Hola Mundo en Javalin :-D"));
+        app.get("/", ctx -> ctx.redirect("/login.html"));
+      /*  app.routes(()->{
+
+        });*/
+
     }
 
 
